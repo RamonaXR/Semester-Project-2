409003319f2b54f162d680b749a96658b792a31f
RamonaXR <Ramonaelise@gmail.com>
2024-08-29 15:30:35 +0200

    adding colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ export default {
   content: ['./**/index.html', './src/**/*.{js,ts,jsx,tsx,mjs,cjs}'],
   theme: {
     extend: {
+      colors: {
+        primary: '#BE1D1D',
+        grayLight: '#D9D9D9',
+        grayLighter: '#E9ECED',
+        dark: '#111111',
+        dark92: '#111111E9', // 92% opacity
+        red: '#DA2828',
+      },
       fontFamily: {
         sans: ['Jura', 'sans-serif'],
       },
